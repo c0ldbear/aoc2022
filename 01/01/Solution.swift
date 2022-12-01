50d81e52282cda7d8285a9db119326bddc3812d9
Teddy Juhlin-Henricson <teddy.juhlinhenricson@daresay.co>
2022-12-01 21:59:37 +0100

    Refactor to use a helper function for both parts

--- 01/01/Solution.swift
+++ 01/01/Solution.swift
@@ -11,37 +11,27 @@ class Solution {
     
     static var shared = Solution()
     
-    func part01(with input: [String.SubSequence]) -> Int {
-        var max = -1
+    private func sumSections(of input: [String.SubSequence]) -> [Int] {
+        var sumSectionsArray = [Int]()
         var sum = 0
         for number in input {
             if let num = Int(number) {
                 sum += num
             } else {
-                if sum > max {
-                    max = sum
-                }
+                sumSectionsArray.append(sum)
                 sum = 0
             }
         }
-        
-        return max
+        return sumSectionsArray
+    }
     
+    func part01(with input: [String.SubSequence]) -> Int {
+        // pick the maximum value
+        return sumSections(of: input).sorted(by: { $0 > $1 }).first ?? -1
     }
     
     func part02(with input: [String.SubSequence]) -> Int {
-        var maxArray = [Int]()
-        var sum = 0
-        for number in input {
-            if let num = Int(number) {
-                sum += num
-            } else {
-                maxArray.append(sum)
-                sum = 0
-            }
-        }
-        
         // pick the sum of top 3
-        return maxArray.sorted(by: {$0 > $1})[0...2].reduce(0, +)
+        return sumSections(of: input).sorted(by: {$0 > $1})[0...2].reduce(0, +)
     }
 }
